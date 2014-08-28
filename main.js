2479422f8e95698c1f343a4eb58cf4f3324f9b69
Ryan Kois <ryan.kois@gmail.com>
2014-08-28 03:03:09 -0700

    Fix bug that prevents image from being rendered on load.

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ var ImageView = function (options) {
 
   var canvas = document.createElement('canvas');
   var picField = document.getElementById(options.picField) || document.getElementById('photo');
+  var img = new Image();
   var photoSelector = document.getElementById(options.photoSelector) || document.getElementById('photo-picker');
   var quality = parseFloat(options.quality) || 0.4;
   var maxSize = parseInt(options.maxSize, 10) || 100;
+  this.hasPic = (photoSelector && photoSelector.files.length >= 1)
 
   this.generate = function (ev, next) {
 
@@ -20,42 +22,48 @@ var ImageView = function (options) {
     }
 
     var picture = ev.target.files[0];
-    var img = new Image();
+    renderImage(picture, next)
+  };
 
+  var onComplete = function (next) {
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    if (canvas.width > maxSize) {
+      var scale = maxSize / canvas.width;
+      canvas.width *= scale;
+      canvas.height *= scale;
+    }
+
+    if (canvas.height > maxSize) {
+      var scale = maxSize / canvas.height;
+      canvas.width *= scale;
+      canvas.height *= scale;
+    }
+
+    canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
+    picField.value = canvas.toDataURL('image/jpeg', quality);
+    preview.appendChild(canvas);
+
+    if (next) {
+      next();
+    }
+
+  };
+
+  var renderImage = function(picture) {
     preview.innerHTML = '';
-
-    var onComplete = function () {
-      canvas.width = img.width;
-      canvas.height = img.height;
-
-      if (canvas.width > maxSize) {
-        var scale = maxSize / canvas.width;
-        canvas.width *= scale;
-        canvas.height *= scale;
-      }
-
-      if (canvas.height > maxSize) {
-        var scale = maxSize / canvas.height;
-        canvas.width *= scale;
-        canvas.height *= scale;
-      }
-
-      canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
-      picField.value = canvas.toDataURL('image/jpeg', quality);
-      preview.appendChild(canvas);
-
-      if (next) {
-        next();
-      }
-    };
-
     img.onload = img.onerror = onComplete;
     img.src = window.URL.createObjectURL(picture);
-  };
+  }
+
 
   this.preview = function () {
     if (photoSelector) {
       photoSelector.addEventListener('change', this.generate, false);
+    }
+    if (photoSelector.files.length >= 1) {
+      renderImage(photoSelector.files[0]);
     }
   };
 };
